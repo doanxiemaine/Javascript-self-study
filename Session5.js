845c85a85e897c1cdf35a90a04b6a5cad8052be4
doanxiemaine <levuongduy0417@gmail.com>
2020-08-10 15:19:22 +0700

    Learned Session 5: Object

---
+++ Session5.js
@@ -0,0 +1,18 @@
+// Object Lesson
+// var a = {
+//     key: value,
+//     key: value
+// }
+
+var myDog = { // camelCase variable naming
+    name: 'Mèo',
+    weight: 5.2,
+    isAlive: false
+};
+
+console.log(myDog);
+
+console.log(myDog.name); //same result as below
+console.log(myDog['name']); //same result as above
+
+var a = {}; //just define it, nothing here yet.
