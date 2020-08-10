8929e6fc4e3666bea0c6f3284f14f0462dc81ef1
doanxiemaine <levuongduy0417@gmail.com>
2020-08-10 14:42:40 +0700

    Learned Session 4: Types

---
+++ Session4.js
@@ -0,0 +1,7 @@
+/*
+    - Primitive types: Number, String, Boolean
+    - Special types: Undefined, Null
+    - Reference types: Object, Array
+    - "Undefined" means declared variable, but have not assign any value to it.
+    - "Null" means declared variable, and assign value "null" to it.
+*/
