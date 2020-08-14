edc6767a64010fb70ba380f0538dade11dd675da
doanxiemaine <levuongduy0417@gmail.com>
2020-08-14 13:51:10 +0700

    Learned Session 40: Callback hell

--- Session40.js
+++ Session40.js
@@ -1,1 +1,13 @@
 // Callback hell
+var fs = require('fs');
+
+fs.readFile('./song1.txt', { encoding: 'utf8' }, function(err, song1) {
+    console.log(song1);
+    fs.readFile('./song2.txt', { encoding: 'utf8' }, function(err, song2) {
+        console.log(song2);
+        fs.readFile('./song3.txt', { encoding: 'utf8' }, function(err, song3) {
+            console.log(song3);
+        });
+    });
+});
+// The code block above is called `Callback hell`, similar to nested for loops or nested if, it is hard for programmers to follow the output of the code.
