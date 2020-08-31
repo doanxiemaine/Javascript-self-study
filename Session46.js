ef32aa4ea622267ce2186f49bc97d41d5185cf32
doanxiemaine <levuongduy0417@gmail.com>
2020-08-31 15:28:32 +0700

    Learned Session 46: setInterval, clearInterval

--- Session46.js
+++ Session46.js
@@ -1,1 +1,24 @@
 // setInterval, clearInterval
+//
+// Syntax:
+// setInterval(function, timeAmount);
+//      ---> Every timeAmount, in ms, passed by, function will be executed once.
+// var i = 0;
+// setInterval(function() {
+//     ++i;
+//     console.log(i);
+// }, 1000); // Count and print out the seconds pass by.
+
+// clearInterval
+//
+// Syntax: same as setInterval
+//      ---> Used to cancel the interval set before. If `setTimeout` returns timeoutId, then `setInterval` returns intervalId.
+// Example:
+var i = 0;
+var intervalId = setInterval(function() {
+    ++i;
+    console.log(i);
+    if (i ===5) {
+        clearInterval(intervalId);
+    }
+}, 1000);
