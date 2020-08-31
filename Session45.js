eea9d9f82049c38937afc4840602cda1c248ec7d
doanxiemaine <levuongduy0417@gmail.com>
2020-08-31 15:09:58 +0700

    Learned Session 45: setTimeout, clearTimeout

--- Session45.js
+++ Session45.js
@@ -1,1 +1,27 @@
 // setTimeout, clearTimeout
+//
+// Syntax: setTimeout(function, timeAmount)
+//      where timeAmount is in milliseconds, function will be executed after timeAmount is up.
+//
+// Example:
+console.log('Start!');
+setTimeout(function() { // anonymous function
+    console.log('Finished!');
+}, 1000);
+console.log('Done!');
+
+// Result of the above block: 
+//      Start!
+//      Done!
+//      Finished!
+// `Finished!` is returned after `Done` since it has been delayed by setTimeout to 1s, or 1000ms.
+//
+// If we don't want to `setTimeout` anymore, we can use `clearTimeout` to cancel the timeout.
+// Since setTimeout() function returns a timeoutId, we will assign the specific setTimeout function we want to clear to a variable.
+// Example:
+console.log('Start counting!');
+var timeoutId = setTimeout(function() { // anonymous function
+    console.log('1...');
+}, 1000);
+console.log('Done!');
+clearTimeout(timeoutId); // '1...' will not be logged in console. Since we have cleared then timeout.
