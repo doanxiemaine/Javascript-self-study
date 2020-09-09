ee08e39a4b03e34a1a796c6447f48949ccc73eeb
doanxiemaine <levuongduy0417@gmail.com>
2020-09-09 10:35:13 +0700

    Done exercise of Session 46: setInterval, clearInterval

--- Session46.js
+++ Session46.js
@@ -14,15 +14,48 @@
 // Syntax: same as setInterval
 //      ---> Used to cancel the interval set before. If `setTimeout` returns timeoutId, then `setInterval` returns intervalId.
 // Example:
-var i = 0;
-var intervalId = setInterval(function() {
-    ++i;
-    console.log(i);
-    if (i ===5) {
-        clearInterval(intervalId);
-    }
-}, 1000);
+// var i = 0;
+// var intervalId = setInterval(function() {
+//     ++i;
+//     console.log(i);
+//     if (i ===5) {
+//         clearInterval(intervalId);
+//     }
+// }, 1000);
 
 // Exercise:
 // Write a function count from 1 to 10
 //      return a promise
+//
+//      -->> Work:
+
+var fs = require('fs');
+
+function countFrom(a, b) {
+    return new Promise(function(resolve, reject) {
+        if (a <= b) {
+            var data = [];
+            var intervalId = setInterval(function() {
+                data.push(a++);
+                if(a === (b+1)) {
+                    clearInterval(intervalId);
+                    resolve(data);
+                }
+            }, 1000);
+        } else {
+            var error = 'Error! Cannot count from ' + a + ' to ' + b;
+            reject(error);
+        }
+        
+    });
+}
+
+countFrom(1, 10).then(function(values) {
+    for (value of values) {
+        console.log(value);
+    }
+    console.log('Done');
+})
+.catch(function(err) {
+    console.log(err);
+});
